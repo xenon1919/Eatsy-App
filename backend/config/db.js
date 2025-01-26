475ee6efc50918c6cfbd5b9077b25d03c0fcc5b9
Ramanchi Rishi Sai Teja <102644611+xenon1919@users.noreply.github.com>
2025-01-26 12:06:08 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Load environment variables from the .env file
+dotenv.config();
 
 export const connectDB = async () => {
-  await mongoose
-    .connect(
-      "mongodb+srv://xenoverse:160821733169@cluster0.zocjo.mongodb.net/food-delivery"
-    )
-    .then(() => console.log("MongoDB connected"));
+  try {
+    await mongoose.connect(process.env.MONGO_URI); //
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit the process with failure
+  }
 };
